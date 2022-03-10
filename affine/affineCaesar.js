1236df6c8c00246989bb297ceef17abb6b5e63ae
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-03-10 14:16:14 +0200

    handle upper cases in affine attack

--- affine/affineCaesar.js
+++ affine/affineCaesar.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
 const plainText = fs.readFileSync("input.txt").toString();
+const keyArr = fs.readFileSync("key.txt").toString().split(";");
+const a = parseInt(keyArr[0]);
+const b = parseInt(keyArr[1]);
 
 // E(x) = a * x + b
 function affineEncrypt(str, a, b) {
   var result = "";
   for (var i = 0; i < str.length; i++) {
     var c = str.charCodeAt(i);
-    if (c >= 97 && c <= 122) {
-      result += String.fromCharCode(97 + (((c - 97) * a + b) % 26));
+    //handle uppercase characters
+    if (c >= 65 && c <= 90) {
+      result += String.fromCharCode(65 + (((c - 65) * a + b) % 26));
+    } else if (c >= 97 && c <= 122) {
+      result += String.fromCharCode((((c - 97) * a + b) % 26) + 97);
     } else {
+      //handle special characters
       result += str.charAt(i);
     }
   }
@@ -31,7 +38,10 @@ function affineDecrypt(str, a, b) {
   var aInv = modInverse(a, 26);
   for (var i = 0; i < str.length; i++) {
     var c = str.charCodeAt(i);
-    if (c >= 97 && c <= 122) {
+    //handle uppercase characters
+    if (c >= 65 && c <= 90) {
+      result += String.fromCharCode(((aInv * (c - 65 - b + 26)) % 26) + 65);
+    } else if (c >= 97 && c <= 122) {
       result += String.fromCharCode(((aInv * (c - 97 - b + 26)) % 26) + 97);
     } else {
       result += str.charAt(i);
@@ -44,11 +54,11 @@ fs.writeFileSync("output.txt", "");
 fs.appendFileSync("output.txt", `plainText:${plainText}\n`);
 fs.appendFileSync(
   "output.txt",
-  `decrypted Result:${affineEncrypt(plainText, 5, 7)}\n`
+  `decrypted Result:${affineEncrypt(plainText, a, b)}\n`
 );
 fs.appendFileSync(
   "output.txt",
-  `decrypted Result:${affineDecrypt(affineEncrypt(plainText, 5, 7), 5, 7)}\n`
+  `decrypted Result:${affineDecrypt(affineEncrypt(plainText, a, b), a, b)}\n`
 );
 
 function getCommandLine() {
