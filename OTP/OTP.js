4e4531e25ab27547a5bf9db498e395511e098f5e
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-03-31 01:13:11 +0200

    row transposition and one time pad

---
+++ OTP/OTP.js
@@ -0,0 +1,113 @@
+/* Hello Dr. Abd El-Ghany, and Dr. Mahmoud
+
+This is an my first attempt to make my code cleaner and have it easily readable. 
+It would be great of you to give me a rating out of 5 and places I can Improve upon. 
+I will try to make use of Helper functions and correct naming of my variables. 
+
+Thank you in advance.
+
+*/
+
+const fs = require("fs");
+const exec = require("child_process").exec;
+
+// HELPER FUNCTIONS
+const legend = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((ch, i) => {
+  return { letter: ch, value: i };
+});
+
+const findKey = (key, param) => {
+  return legend.find((la) => la[param] === key);
+};
+
+const search = (key) => {
+  const found = findKey(key, "letter");
+  if (!found) {
+    return "";
+  }
+  return found.value;
+};
+
+const reverse = (key) => {
+  const found = findKey(key, "value");
+  if (!found) {
+    return "";
+  }
+  return found.letter;
+};
+
+const mod = (n) => {
+  const p = legend.length - 1;
+  return n - p * Math.floor(n / p);
+};
+const clean = (text) => {
+  // Handle white spaces and convert to upper text
+  return text.replace(/\s/g, "").toUpperCase();
+};
+
+// MAIN  FUNCTIONS
+function encrypt(message, key) {
+  let encryptedMessage = "";
+  message = clean(message);
+  key = clean(key);
+  for (let i = 0; i < message.length; i++) {
+    const valueString = search(message[i]);
+    const valueKey = search(key[i]);
+    const intString =
+      typeof valueString === "undefined" ? parseInt(message[i]) : valueString;
+    const intKey =
+      typeof valueKey === "undefined" ? parseInt(key[i]) : valueKey;
+    if (isNaN(intString) || isNaN(intKey)) {
+      encryptedMessage += message[i];
+    } else {
+      const reverseValue = mod(intString + intKey);
+      const reverseLookup = reverse(reverseValue);
+      if (reverseLookup == 0) {
+        reverseLookup = "";
+      }
+      encryptedMessage += reverseLookup;
+    }
+  }
+  return encryptedMessage;
+}
+
+function decrypt(message, key) {
+  let encryptedMessage = "";
+  message = clean(message);
+  key = clean(key);
+  for (let i = 0; i < message.length; i++) {
+    const valueString = search(message[i]);
+    const valueKey = search(key[i]);
+    const intString =
+      typeof valueString === "undefined" ? parseInt(message[i]) : valueString;
+    const intKey =
+      typeof valueKey === "undefined" ? parseInt(key[i]) : valueKey;
+    if (isNaN(intString) || isNaN(intKey)) {
+      encryptedMessage += message[i];
+    } else {
+      const reverseValue = mod(intString - intKey);
+      let reverseLookup = reverse(reverseValue);
+      if (reverseLookup == 0) {
+        reverseLookup = "";
+      }
+      encryptedMessage += reverseLookup;
+    }
+  }
+  return encryptedMessage;
+}
+
+// DRIVER FUNCTION
+function main() {
+  const plainText = fs.readFileSync("plainText.txt").toString();
+  const key = fs.readFileSync("key.txt").toString();
+  const cipherText = fs.readFileSync("cipherText.txt").toString();
+
+  const encrypted = encrypt(plainText, key);
+  const decrypted = decrypt(cipherText, key);
+
+  fs.writeFileSync("output.txt", `${encrypted}\n${decrypted}`);
+  // open the file in the default txt viewer  (notepad)
+  exec("start output.txt");
+}
+
+main();
--- RowTrans/RowTransposition.js
+++ RowTrans/RowTransposition.js
@@ -1,30 +1,43 @@
-var chars = "abcdefghijklmnopqrstuvwxyz";
+/* Hello Dr. Abd El-Ghany, and Dr. Mahmoud
 
-function handleEncrypt() {
-  var plaintext = normalize(getById("p").value);
-  if (validate(plaintext, "Please enter some plaintext.")) return;
-  var key = normalize(getById("key").value);
-  var pc = normalize(getById("pc").value);
-  getById("c").value = Encrypt(plaintext, key, pc);
-}
-function handleDecrypt() {
-  var ciphertext = normalize(getById("c").value);
-  if (validate(ciphertext, "Please enter some ciphertext (letters only)."))
-    return;
-  var key = normalize(getById("key").value);
-  getById("p").value = Decrypt(ciphertext, key);
-}
+This is an my first attempt to make my code cleaner and have it easily readable. 
+It would be great of you to give me a rating out of 5 and places I can Improve upon. 
+I will try to make use of Helper functions and correct naming of my variables. 
+
+Thank you in advance.
+
+*/
+
+const fs = require("fs");
+const exec = require("child_process").exec;
+const chars = "abcdefghijklmnopqrstuvwxyz";
+
+// HELPER FUNCTIONS
+
+const validate = (text, message) => {
+  if (text.length < 1) {
+    alert(message);
+  }
+};
+
+const getById = (id) => {
+  return document.getElementById(id);
+};
+
+const normalize = (value) => {
+  return value.toLowerCase().replace(/[^a-z]/g, "");
+};
 
 function Encrypt(plaintext, key, pc) {
-  var klen = key.length;
+  const keyLength = key.length;
   if (pc == "") pc = "x";
-  while (plaintext.length % klen != 0) {
+  while (plaintext.length % keyLength != 0) {
     plaintext += pc.charAt(0);
   }
-  var colLength = plaintext.length / klen;
-  var ciphertext = "";
+  let colLength = plaintext.length / keyLength;
+  let ciphertext = "";
   k = 0;
-  for (i = 0; i < klen; i++) {
+  for (i = 0; i < keyLength; i++) {
     while (k < 26) {
       t = key.indexOf(chars.charAt(k));
       arrkw = key.split("");
@@ -34,33 +47,33 @@ function Encrypt(plaintext, key, pc) {
       else k++;
     }
     for (j = 0; j < colLength; j++) {
-      ciphertext += plaintext.charAt(j * klen + t);
+      ciphertext += plaintext.charAt(j * keyLength + t);
     }
   }
   return ciphertext;
 }
 
 function Decrypt(ciphertext, keyword) {
-  var klen = keyword.length;
-  if (klen <= 1) {
+  let keyLength = keyword.length;
+  if (keyLength <= 1) {
     alert("keyword should be at least 2 characters long");
     return;
   }
-  if (ciphertext.length % klen != 0) {
+  if (ciphertext.length % keyLength != 0) {
     alert(
       "ciphertext has not been padded, the result may be incorrect (incorrect keyword?)."
     );
   }
   // first we put the text into columns based on keyword length
-  var cols = new Array(klen);
-  var colLength = ciphertext.length / klen;
-  for (i = 0; i < klen; i++)
+  let cols = new Array(keyLength);
+  let colLength = ciphertext.length / keyLength;
+  for (i = 0; i < keyLength; i++)
     cols[i] = ciphertext.substr(i * colLength, colLength);
   // now we rearrange the columns so that they are in their unscrambled state
-  var newcols = new Array(klen);
+  let newcols = new Array(keyLength);
   j = 0;
   i = 0;
-  while (j < klen) {
+  while (j < keyLength) {
     t = keyword.indexOf(chars.charAt(i));
     if (t >= 0) {
       newcols[t] = cols[j++];
@@ -70,25 +83,44 @@ function Decrypt(ciphertext, keyword) {
     } else i++;
   }
   // now read off the columns row-wise
-  var plaintext = "";
+  let plaintext = "";
   for (i = 0; i < colLength; i++) {
-    for (j = 0; j < klen; j++) {
+    for (j = 0; j < keyLength; j++) {
       plaintext += newcols[j].charAt(i);
     }
   }
   return plaintext;
 }
 
-function validate(text, message) {
-  if (text.length < 1) {
-    alert(message);
-  }
+// Main functions
+function handleEncrypt() {
+  let plaintext = normalize(getById("p").value);
+  if (validate(plaintext, "Please enter some plaintext.")) return;
+  let key = normalize(getById("key").value);
+  let pc = normalize(getById("pc").value);
+  getById("c").value = Encrypt(plaintext, key, pc);
+}
+function handleDecrypt() {
+  let ciphertext = normalize(getById("c").value);
+  if (validate(ciphertext, "Please enter some ciphertext (letters only)."))
+    return;
+  let key = normalize(getById("key").value);
+  getById("p").value = Decrypt(ciphertext, key);
 }
 
-function getById(id) {
-  return document.getElementById(id);
-}
+//! USE FOR TESTING. I recommend you run the html file in your browser for a simple GUI to test with.
+// // DRIVER FUNCTION
+// function main() {
+//   const plainText = fs.readFileSync("plainText.txt").toString();
+//   const key = fs.readFileSync("key.txt").toString();
+//   const cipherText = fs.readFileSync("cipherText.txt").toString();
 
-function normalize(value) {
-  return value.toLowerCase().replace(/[^a-z]/g, "");
-}
+//   const encrypted = encrypt(plainText, key);
+//   const decrypted = decrypt(cipherText, key);
+
+//   fs.writeFileSync("output.txt", `${encrypted}\n${decrypted}`);
+//   // open the file in the default txt viewer  (notepad)
+//   exec("start output.txt");
+// }
+
+// main();
