99c26f3031368fbf671c2400a04b7045c63f736b
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-05-26 21:35:20 +0200

    ...

--- Des/DesKey_1827239.js
+++ Des/DesKey_1827239.js
@@ -1,17 +1,115 @@
 const fs = require("fs");
 const pc_1 = [
-  57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35,
-  27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38,
-  30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4,
+  57,
+  49,
+  41,
+  33,
+  25,
+  17,
+  9,
+  1,
+  58,
+  50,
+  42,
+  34,
+  26,
+  18,
+  10,
+  2,
+  59,
+  51,
+  43,
+  35,
+  27,
+  19,
+  11,
+  3,
+  60,
+  52,
+  44,
+  36,
+  63,
+  55,
+  47,
+  39,
+  31,
+  23,
+  15,
+  7,
+  62,
+  54,
+  46,
+  38,
+  30,
+  22,
+  14,
+  6,
+  61,
+  53,
+  45,
+  37,
+  29,
+  21,
+  13,
+  5,
+  28,
+  20,
+  12,
+  4,
 ];
 
 const pc_2 = [
-  14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27,
-  20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34,
-  53, 46, 42, 50, 36, 29, 32,
+  14,
+  17,
+  11,
+  24,
+  1,
+  5,
+  3,
+  28,
+  15,
+  6,
+  21,
+  10,
+  23,
+  19,
+  12,
+  4,
+  26,
+  8,
+  16,
+  7,
+  27,
+  20,
+  13,
+  2,
+  41,
+  52,
+  31,
+  37,
+  47,
+  55,
+  30,
+  40,
+  51,
+  45,
+  33,
+  48,
+  44,
+  49,
+  39,
+  56,
+  34,
+  53,
+  46,
+  42,
+  50,
+  36,
+  29,
+  32,
 ];
 
-const leftShift = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1];
+const leftShift = [ 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 ];
 
 let binaryNumber = "";
 let leftKey = "";
@@ -24,7 +122,7 @@ let temp = " ";
 function bin2hex(s) {
   let result = "";
   let i = 0;
-  if (s.length % 4) s = "0".repeat(4 - (s.length % 4)) + s;
+  if (s.length % 4) s = "0".repeat(4 - s.length % 4) + s;
   while (i < s.length) {
     result += parseInt(s.substring(i, i + 4), 2).toString(16);
     i += 4;
@@ -45,7 +143,7 @@ function leftShiftKey(numOfShifts) {
 }
 
 // DesKey function
- function DesKey(keyFile) {
+function DesKey(keyFile) {
   let readKeyFile = fs.readFileSync(keyFile, "utf8");
   let keyString = readKeyFile.split("\n")[0];
   let hexadecimalNumber = "";
@@ -63,19 +161,16 @@ function leftShiftKey(numOfShifts) {
   let fileDir = process.cwd() + "/generatedKeys.txt";
   let generatedKeysFile = fs.openSync(fileDir, "w");
   for (let i = 1; i <= 16; i++) {
-    fs.writeFileSync(
-      generatedKeysFile,
-      "KEY[" + i + "]\n",
-      "utf8",
-      function (err) {
-        if (err) throw err;
-      }
-    );
+    fs.writeFileSync(generatedKeysFile, "KEY[" + i + "]\n", "utf8", function(
+      err
+    ) {
+      if (err) throw err;
+    });
     fs.writeFileSync(
       generatedKeysFile,
       bin2hex(leftShiftKey(leftShift[i])) + "\n",
       "utf8",
-      function (err) {
+      function(err) {
         if (err) throw err;
       }
     );
@@ -87,7 +182,7 @@ function leftShiftKey(numOfShifts) {
 function main() {
   let file = process.cwd() + "/key.txt";
   let keyFile = fs.openSync(file, "w");
-  fs.writeFileSync(keyFile, "0f57c907d9e859", "utf8", function (err) {
+  fs.writeFileSync(keyFile, "0f57c907d9e859", "utf8", function(err) {
     if (err) throw err;
   });
   fs.closeSync(keyFile);
@@ -95,6 +190,5 @@ function main() {
 }
 
 main();
-
 
 module.exports = { DesKey };
